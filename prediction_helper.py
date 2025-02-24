1db6bfde3c76b400bde475b38390e3ced148d516
aliterki <aliterki349@gmail.com>
2025-02-24 05:51:51 -0600

    update file path

--- prediction_helper.py
+++ prediction_helper.py
@@ -3,10 +3,10 @@ from joblib import load
 
 
 
-model_rest = load("artifacts\model_rest.joblib")
-model_young = load("artifacts\model_young.joblib")
-scaler_rest = load("artifacts\scaler_rest.joblib")
-scaler_young = load("artifacts\scaler_young.joblib")
+model_rest = load("artifacts/model_rest.joblib")
+model_young = load("artifacts/model_young.joblib")
+scaler_rest = load("artifacts/scaler_rest.joblib")
+scaler_young = load("artifacts/scaler_young.joblib")
 
 
 
